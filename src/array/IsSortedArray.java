1b0cdf4177a9594d97f3ec4ac188a3105b840e7b
akash-55 <akash.20444@gmail.com>
2022-06-16 01:15:35 +0530

    array 2

---
+++ src/array/IsSortedArray.java
@@ -0,0 +1,23 @@
+package array;
+
+public class IsSortedArray {
+	static boolean isSorted(int arr[], int n)
+    {
+    	for(int i = 1; i < n; i++)
+    	{
+    	    if(arr[i] < arr[i - 1])
+    	        return false;
+    	}
+
+    	return true;
+    } 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {5, 12, 30, 2, 35}, n = 5;
+
+	       System.out.println(isSorted(arr, n));
+
+	}
+
+}
